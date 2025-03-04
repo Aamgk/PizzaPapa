963628441252f16a1b64c5a5f495bf78fb12b11e
Aamgk <osiilen@bk.ru>
2025-03-04 11:33:55 +0300

    Initial commit

---
+++ src/main/java/com/modsen/pizzap/PizzaPApplication.java
@@ -0,0 +1,13 @@
+package com.modsen.pizzap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PizzaPApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PizzaPApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/modsen/pizzap/PizzaPApplicationTests.java
@@ -0,0 +1,13 @@
+package com.modsen.pizzap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PizzaPApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
